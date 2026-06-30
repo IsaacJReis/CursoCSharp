d365acf96cc4d480062aad8df7276d5710618a97
Isaac Reis <isaacreis209@gmail.com>
2026-06-30 12:15:18 -0300

    Metodos Estaticos

--- CursoCSharp/ClassesEMetodos/MetodosEstaticos.cs
+++ CursoCSharp/ClassesEMetodos/MetodosEstaticos.cs
@@ -10,7 +10,7 @@ namespace CursoCSharp.ClassesEMetodos {
 
         public class CalculadoraEstatica {
 
-            public  int Somar(int a, int b) {
+            public int Somar(int a, int b) {
                 return a + b;
 
             }
@@ -22,16 +22,14 @@ namespace CursoCSharp.ClassesEMetodos {
 
         }
 
-
-
         public static void Executar() {
 
             var resultado = CalculadoraEstatica.Multiplicar(10, 25);
-            Console.WriteLine("Resultado: {0}", resultado ); // Assecivel a partir da classe, sem precisar criar um objeto
+            Console.WriteLine("Resultado: {0}", resultado); // Assecivel a partir da classe, sem precisar criar um objeto
 
 
             CalculadoraEstatica calc = new CalculadoraEstatica();
-            Console.WriteLine(calc.Somar(3,5)); // Somar é um método de instância, ou seja, precisa criar um objeto para acessar
+            Console.WriteLine(calc.Somar(3, 5)); // Somar é um método de instância, ou seja, precisa criar um objeto para acessar
         }
     }
 }
